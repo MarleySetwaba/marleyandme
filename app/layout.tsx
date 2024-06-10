698b884e63b15bb642625ca841df5ec3ae36e64d
MarleySetwaba <73256020+MarleySetwaba@users.noreply.github.com>
2024-06-10 17:54:38 +0200

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Nav/>
         {children}
         <Footer/>
+        <Analytics/>
         </body>
     </html>
   );
