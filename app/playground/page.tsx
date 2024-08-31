cbc6d76f75feaf5661f5db38cf8212c1cfd99d5c
MarleySetwaba <73256020+MarleySetwaba@users.noreply.github.com>
2024-08-31 09:28:56 +0200

    New Blog

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,13 +1,56 @@
-import Schedule from '@/components/Schedule'
-import React from 'react'
+'use client';
 
-const page = () => {
+import { useState } from 'react';
+import { Message, continueConversation } from './actions';
+import { readStreamableValue } from 'ai/rsc';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export default function Home() {
+  const [conversation, setConversation] = useState<Message[]>([]);
+  const [input, setInput] = useState<string>('');
+
   return (
     <div>
-    <h2 className='text-blue-600 text-2xl text-center'>Schedule</h2>
-    <Schedule start_date='2024-07-26'/>
-    </div>
-  )
-}
+      <div>
+        {conversation.map((message, index) => (
+          <div key={index}>
+            {message.role}: {message.content}
+          </div>
+        ))}
+      </div>
 
-export default page
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={event => {
+            setInput(event.target.value);
+          }}
+        />
+        <button
+          onClick={async () => {
+            const { messages, newMessage } = await continueConversation([
+              ...conversation,
+              { role: 'user', content: input },
+            ]);
+
+            let textContent = '';
+
+            for await (const delta of readStreamableValue(newMessage)) {
+              textContent = `${textContent}${delta}`;
+
+              setConversation([
+                ...messages,
+                { role: 'assistant', content: textContent },
+              ]);
+            }
+          }}
+        >
+          Send Message
+        </button>
+      </div>
+    </div>
+  );
+}
